d8ac51d6ccd76e203b9e87262c3bcdf6eb5e0ea1
VITOR DE ASSIS PASSOS <vitor.apassos@senacsp.edu.br>
2025-05-20 22:09:37 -0300

    Implementado TAILWIND

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 
 console.log("Executando Processo Principal")
 
-const { app, BrowserWindow, nativeTheme, Menu } = require("electron/main")
+const { app, BrowserWindow, nativeTheme, Menu, ipcMain } = require("electron/main")
 
 let win
 
@@ -16,12 +16,66 @@ const createWindow = () => {
         height: 720,
 
     })
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template))
     win.loadFile("./src/views/index.html")
-
 }
 
 app.whenReady().then(() => {
-createWindow();
-    
-
+    createWindow();
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+            console.log("Passou pelo if browser")
+        }
+    })
 });
+
+const template = [
+    {
+        label: "Cadastro",
+        submenu: [
+            {
+                label: "Cadastrar",
+                click: () => cadastroWindow(),
+            },
+            {
+                label: "Sair",
+                click: () => console.log("close()")
+            }
+        ]
+    },
+    {
+        label: "Relatórios",
+        submenu: [
+            {
+                label: "Clientes",
+                click: () => showClientReports()
+            },
+            {
+                label: "OS's Abertas",
+                click: () => showOpenOSReports()
+            },
+            {
+                label: "OS's Fechadas",
+                click: () => showClosedOSReports()
+            },
+            {
+                label: "Todas OS",
+                click: () => showAllOSReports()
+            }
+        ]
+    },
+    {
+        label:"Ajuda",
+        submenu:[
+            {
+                label:"Repositório",
+                click: () => console.log("incluir o redirecionador https://github.com/vitorapassos/grafica-os.git")
+            },
+            {
+                label: "Sobre",
+                click: () => aboutWindow()
+            }
+        ]
+    }
+]
